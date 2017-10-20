20bffd481d9d6221ea1f2824f9915e10bf263d4a
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-20 07:22:30 +0200

    test: go tapeless

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const { ok } = require('assert')
+const { ok } = require('tapeless')
 const { equal, notEqual } = require('./')
 
 const t1 = equal(2, 2)
 const t2 = notEqual(2, 3)
 
-ok(t1, 'equal')
+ok(t1, 'equal', 'will compute')
 ok(t2, 'not equal')
