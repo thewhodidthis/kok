a9e9d43ab313e14e00ae18b22bc9c9e1ab129ad5
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-02-22 16:17:38 +0000

    test: cleanup

--- test.js
+++ test.js
@@ -4,9 +4,11 @@ const { equal, ok } = require('assert')
 const { assert } = require('./')
 
 const t1 = assert.equal(2, 2)
-const t2 = assert.notEqual(2, 3)
 
 equal(assert.equal.name, 'same')
 ok(t1)
+
+const t2 = assert.notEqual(2, 3)
+
 equal(assert.notEqual.name, 'different')
 ok(t2)
