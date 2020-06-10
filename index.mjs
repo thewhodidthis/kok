d2ec7dba5d32a2726d2fc2a4dcaf8a047e59c74d
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2020-06-10 16:03:37 +0100

    module: name the id function default, fix naming, cleanup, rebuild cjs

--- index.mjs
+++ index.mjs
@@ -1,33 +1,53 @@
-// Helps create assertions?
-const reassert = (assert = v => v, expected, operator = 'is') => {
+// # Likewise
+// Helps create assertions; provides a few of them to begin with
+
+// Creates an assertion by wrapping a given function, "expected" value, and "operator" key
+// to produce a custom `Error` if the original function fails when called
+function reassert(assert = identity, expectedMaybe, operator = 'is') {
+  // Attempt to extract a name for the would be assertion; could be using the
+  // optional chaining operator in ES2020, eg. `assert?.name`
+  const name = assert && assert.name
+
+  // Throws if the function being wrapped fails
   const test = (...assertion) => {
+    // Run test and pass on the result if truthy
     const result = assert(...assertion)
 
-    if (!result) {
-      // Cook up own exception
-      throw Object.assign(Error(name), { actual: assertion.shift(), expected, operator })
+    if (result) {
+      return result
     }
 
-    return result
+    // Fill in a few details cooking up own exception if the result is falsy
+    const [actual, expected = expectedMaybe] = assertion
+
+    throw Object.assign(Error(name), { actual, expected, operator })
   }
 
-  // Preserve name
+  // Preserve name of function being wrapped
   Object.defineProperty(test, 'name', {
-    value: assert && assert.name,
+    value: name,
     configurable: true
   })
 
   return test
 }
 
+function identity(v) {
+  return v
+}
+
+// Checks for truthiness by default when no function is fed in
+// https://2ality.com/2013/04/quirk-implicit-conversion.html
 const assert = reassert()
 
+// Add named checks for truthiness
 const truthy = a => !!a
 const falsy = a => !a
 
 assert.ok = reassert(truthy, true, '!!')
 assert.notOk = reassert(falsy, false, '!')
 
+// Same for equality
 const same = (a, b) => Object.is(a, b)
 const different = (a, b) => !same(a, b)
 
